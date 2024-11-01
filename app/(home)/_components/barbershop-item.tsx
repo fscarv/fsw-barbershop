6aa64901eb0e15615e7699247678190bb7fc78b0
scarv <fernando.carvalho@lcsistemas.com.br>
2024-11-01 10:20:44 -0300

    feat: add ServiceItem component

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,22 +1,31 @@
+"use client";
+
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Barbershop } from "@prisma/client";
 import { StarIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface BarbershopItemProps {
     barbershop: Barbershop;
 }
 
-const BarbershopItem: React.FC<BarbershopItemProps> = ({ barbershop }) => {
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+    const router = useRouter();
+    
+    const handleBookingClick = () => {
+        router.push(`barbershops/${barbershop.id}`);
+    }
+
     return (
         <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
             <CardContent className="px-1 py-0">
                 <div className="w-full h-[159px] relative">
                     <div className="absolute top-2 left-2 z-50">
                         <Badge variant={"secondary"} className="opacity-90 flex gap-1 items-center top-3 left-3">
-                            <StarIcon size={12} className="fill-primary text-primary"/>
+                            <StarIcon size={12} className="fill-primary text-primary" />
                             <span className="text-xs">5,0</span>
                         </Badge>
                     </div>
@@ -33,7 +42,13 @@ const BarbershopItem: React.FC<BarbershopItemProps> = ({ barbershop }) => {
                 <div className="px-3 pb-3">
                     <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
                     <p className="text-sm text-gray-400 ">{barbershop.address}</p>
-                    <Button className="w-full mt-3" variant={"secondary"}>Agendar</Button>
+                    <Button
+                        className="w-full mt-3 text-violet-600"
+                        variant={"secondary"}
+                        onClick={handleBookingClick}
+                    >
+                        Agendar
+                    </Button>
                 </div>
             </CardContent>
         </Card>
