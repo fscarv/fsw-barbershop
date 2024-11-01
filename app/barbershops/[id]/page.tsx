203ecc20615fe74f9ca1753b468c3faf0c086e7f
scarv <fernando.carvalho@lcsistemas.com.br>
2024-11-01 11:39:19 -0300

    fix: add next auth setup

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -3,42 +3,42 @@ import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
 
 interface BarbershopDetailsPageProps {
-    params: {
-        id?: string;
-    }
+  params: {
+    id?: string;
+  }
 }
 
 const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => {
-    if (!params.id) {
-        // TODO: redirecionar para home page
-        return null;
-    }
+  if (!params.id) {
+    // TODO: redirecionar para home page
+    return null;
+  }
 
-    const barbershop = await db.barbershop.findUnique({
-        where: {
-            id: params.id,
-        },
-        include: {
-            services: true,
-        },
-    });
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  });
 
-    if (!barbershop) {
-        // TODO: redirecionar para home page
-        return null;
-    }
+  if (!barbershop) {
+    // TODO: redirecionar para home page
+    return null;
+  }
 
-    return (
-        <div>
-            <BarbershopInfo barbershop={barbershop} />
-            <div className="px-5 flex flex-col gap-4 py-6">
-                {barbershop.services.map((service) => (
-                    <ServiceItem key={service.id} service={service} />
-                ))}
-            </div>
+  return (
+    <div>
+      <BarbershopInfo barbershop={barbershop} />
+      <div className="px-5 flex flex-col gap-4 py-6">
+        {barbershop.services.map((service) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
 
-        </div>
-    );
+    </div>
+  );
 };
 
 export default BarbershopDetailsPage;
